f73a2d28df79bab5f094c7df5ab97f5984837464
Ranvijay <ranvijay.gehlaut@mail.vinove.com>
2022-09-08 18:46:18 +0530

    hello

--- dateofbirth/views.py
+++ dateofbirth/views.py
@@ -1,5 +1,22 @@
+from ast import arg
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import  JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+from datetime import date
 
-def say_hello(request):
-    return render(request,'hello.html',{'name:Ranvijay'})
+@api_view(['POST'])
+def calculateAge(birthDate):
+    try:
+        days_in_year = 365.2425   
+        age = int((date.today() - birthDate).days / days_in_year)
+        return JsonResponse(calculateAge(date(1993,2,3),"years")
+    except: ValueError as e:
+        return Response(e.arg[0],status.HTTP_400_BAD_REQUEST)
+
